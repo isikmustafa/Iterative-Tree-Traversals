7890e762022e3fb94d0b7c668aed9e11687861b9
Mustafa Işık <yazarblog@gmail.com>
2015-02-03 01:58:32 +0200

    Create postorder.cpp

---
+++ postorder.cpp
@@ -0,0 +1,36 @@
+struct Dual
+{
+	BinaryNode *ptr;
+	bool flag;
+
+	Dual(BinaryNode *p = nullptr, bool f = false)
+		: ptr(p)
+		, flag(f)
+	{}
+};
+
+void BinaryTree::postorder()
+{
+	stack<Dual> s;
+	s.push(Dual(root, false));
+
+	Dual temp;
+	while (!s.empty())
+	{
+		temp = s.top();
+		s.pop();
+
+		if (temp.ptr == nullptr); //DO NOTHING!
+
+		//visit the current node
+		else if (temp.flag)
+			cout << temp.ptr->data << endl;
+
+		else
+		{
+			s.push(Dual(temp.ptr, true));
+			s.push(Dual(temp.ptr->right, false));
+			s.push(Dual(temp.ptr->left, false));
+		}
+	}
+}
